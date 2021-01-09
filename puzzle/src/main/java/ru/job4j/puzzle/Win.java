b9c4621e6815b6c7a49da55b56107037d3824426
Xazeq <setezey@gmail.com>
2021-01-09 14:32:24 +0300

    2. Сокобан. Тестовое задание. [#433351]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -5,7 +5,7 @@ public class Win {
         boolean result = false;
         for (int index = 0; index < board.length; index++) {
             if (board[index][index] == 1) {
-                if (monoHorizontal(board) || monoVertical(board)) {
+                if (monoHorizontal(board, index) || monoVertical(board, index)) {
                     result = true;
                     break;
                 }
@@ -14,35 +14,23 @@ public class Win {
         return result;
     }
 
-    public static boolean monoHorizontal(int[][] board) {
+    public static boolean monoHorizontal(int[][] board, int row) {
         boolean result = true;
-        for (int row = 0; row < board.length; row++) {
-            result = true;
-            for (int column = 0; column < board.length; column++) {
-                if (board[row][column] != 1) {
+        for (int index = 0; index < board.length; index++) {
+                if (board[row][index] != 1) {
                     result = false;
                     break;
-                }
-            }
-            if (result) {
-                break;
             }
         }
         return result;
     }
 
-    public static boolean monoVertical(int[][] board) {
+    public static boolean monoVertical(int[][] board, int column) {
         boolean result = true;
-        for (int row = 0; row < board.length; row++) {
-            result = true;
-            for (int column = 0; column < board.length; column++) {
-                if (board[column][row] != 1) {
+        for (int index = 0; index < board.length; index++) {
+                if (board[index][column] != 1) {
                     result = false;
                     break;
-                }
-            }
-            if (result) {
-                break;
             }
         }
         return result;
